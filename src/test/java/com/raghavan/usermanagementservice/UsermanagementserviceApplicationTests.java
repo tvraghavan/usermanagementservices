9ced4724768ba8aec2cb292c722d2a71d8841572
tvraghavan <tvraghavan@gmail.com>
2020-11-01 00:33:32 +0530

    baesline

---
+++ src/test/java/com/raghavan/usermanagementservice/UsermanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghavan.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
